3378f3f2b7d33f4e5ece98d7793b478bf4309398
nesmun <jesus.emc@hotmail.com>
2023-09-17 12:31:38 -0600

    removed admin_id from service model

--- src/Controllers/serviceControllers.js
+++ src/Controllers/serviceControllers.js
@@ -1,40 +1,40 @@
 const { Service } = require("../db");
-const {Op} = require("sequelize")
+const { Op } = require("sequelize");
 
 const postServiceController = async (
-  type,
-  name,
-  description,
-  provider,
-  price,
-  image,
-  status
-) => {
-  const newService = await Service.create({
     type,
     name,
     description,
     provider,
     price,
     image,
-    status,
-  });
-  return newService;
+    status
+) => {
+    const newService = await Service.create({
+        type,
+        name,
+        description,
+        provider,
+        price,
+        image,
+        status,
+    });
+    return newService;
 };
 const getServiceByNameController = async (name) => {
-  const tolowerCaseName = name.toLowerCase();
-  const service = await Service.findAll({
-    where: { name: { [Op.iLike]: "%" + tolowerCaseName + "%" } },
-  });
-  return service
+    const tolowerCaseName = name.toLowerCase();
+    const service = await Service.findAll({
+        where: { name: { [Op.iLike]: "%" + tolowerCaseName + "%" } },
+    });
+    return service;
 };
 
 const gelAllServicesController = async () => {
-  return await Service.findAll();
+    return await Service.findAll();
 };
 
 module.exports = {
-  postServiceController,
-  getServiceByNameController,
-  gelAllServicesController,
+    postServiceController,
+    getServiceByNameController,
+    gelAllServicesController,
 };
