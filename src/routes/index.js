bdb97dfc52e32910cc3b524475594c426b8a64e1
fgvies <facundovies@gmail.com>
2023-09-20 00:21:14 -0300

    Avanzando en algunas rutas para las ofertas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,17 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const router = Router();
 
-const userrouter = require('./usersRoutes');
+const userrouter = require("./usersRoutes");
 
-const serviceRouter= require("./serviceRoutes")
-const reviewRouter = require('./reviewRoutes');
+const serviceRouter = require("./serviceRoutes");
+const reviewRouter = require("./reviewRoutes");
 const contactRouter = require("./contactRoutes");
+const offerRouter = require("./offerRoutes");
 
-router.use('/users', userrouter);
-router.use('/services', serviceRouter);
-router.use('/reviews', reviewRouter);
+router.use("/users", userrouter);
+router.use("/services", serviceRouter);
+router.use("/reviews", reviewRouter);
 router.use("/contact", contactRouter);
-
+router.use("/offer", offerRouter);
 
 module.exports = router;
