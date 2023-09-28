f6a9004be809b4e37c915e3308bc0b339859ff27
nesmun <jesus.emc@hotmail.com>
2023-09-28 14:47:58 -0600

    coded: create, delete, restore, update, get, getById,

--- src/Handlers/packHandlers.js
+++ src/Handlers/packHandlers.js
@@ -1,13 +1,71 @@
-const { createPackController } = require("../Controllers/packController.js");
+const {
+    createPackController,
+    deletePackController,
+    restorePackController,
+    updatePackController,
+    getPacksController,
+    getPackByIdController,
+} = require("../Controllers/packController.js");
 
 const createPack = async (req, res) => {
     try {
-        await createPackController(req);
+        const { message, pack } = await createPackController(req);
+        res.status(201).json({ message, pack });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+const deletePack = async (req, res) => {
+    try {
+        const { message, pack } = await deletePackController(req);
+        res.status(200).json({ message, pack });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+const restorePack = async (req, res) => {
+    try {
+        const { message, pack } = await restorePackController(req);
+        res.status(200).json({ message, pack });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+const updatePack = async (req, res) => {
+    try {
+        const { message, pack } = await updatePackController(req);
+        res.status(200).json({ message, pack });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+const getPacks = async (req, res) => {
+    try {
+        const { message, packs } = await getPacksController(req);
+        res.status(200).json({ message, packs });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+const getPackById = async (req, res) => {
+    try {
+        const { message, pack } = await getPackByIdController(req);
+        res.status(200).json({ message, pack });
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
 };
 
 module.exports = {
+    getPacks,
     createPack,
+    getPackById,
+    updatePack,
+    restorePack,
+    deletePack,
 };
