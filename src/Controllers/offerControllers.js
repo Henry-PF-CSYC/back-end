968b804b2217157a9f5e554875b008ae2efe1cf9
jeermuce <ernesto_mc.dev@proton.me>
2023-09-27 19:53:48 -0600

    offerControllers patch, only admins get deleted publications

--- src/Controllers/offerControllers.js
+++ src/Controllers/offerControllers.js
@@ -2,14 +2,18 @@ const { Offer } = require("../db");
 const { filterOrderAndPaginateOffers } = require("../utils/offerUtils");
 const { Op } = require("sequelize");
 const getAllOfferController = async (req) => {
-    const { title, type, order, orderBy, page, size } = req.query;
+    const { title, type, order, orderBy, page, size, user_type } = req.query;
     let offers = [];
+    let is_paranoid = true;
+    if (user_type === "admin" || user_type === "contact_admin ")
+        is_paranoid = false;
+    else is_paranoid = true;
     if (title)
         offers = await Offer.findAll({
             where: { title: { [Op.iLike]: `%${title}%` } },
             paranoid: false,
         });
-    else offers = await Offer.findAll({ paranoid: false });
+    else offers = await Offer.findAll({ paranoid: is_paranoid });
     const { totalPages, paginated } = await filterOrderAndPaginateOffers(
         offers,
         type,
