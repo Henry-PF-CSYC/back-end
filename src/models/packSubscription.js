c5653299f7818d2441b483f1b77b8ae782cc2db2
nesmun <jesus.emc@hotmail.com>
2023-09-28 22:13:41 -0600

    fix/packSubscription model

--- src/models/packSubscription.js
+++ src/models/packSubscription.js
@@ -2,7 +2,7 @@ const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
     sequelize.define(
-        "subscription",
+        "packSubscription",
         {
             id: {
                 primaryKey: true,
