24d8ed4352d5290f263339450e6816ce6751b0f9
nesmun <jesus.emc@hotmail.com>
2023-09-28 22:09:02 -0600

    created packSubscription model

---
+++ src/models/packSubscription.js
@@ -0,0 +1,45 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+    sequelize.define(
+        "subscription",
+        {
+            id: {
+                primaryKey: true,
+                type: DataTypes.UUID,
+                defaultValue: DataTypes.UUIDV4,
+            },
+            due_date: {
+                type: DataTypes.DATE,
+                allowNull: false,
+            },
+            deletedAt: {
+                type: DataTypes.DATE,
+                allowNull: true,
+            },
+            status: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            user_id: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            pack_id: {
+                type: DataTypes.UUID,
+                allowNull: false,
+            },
+            service_name: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            user_pack_pair: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+        },
+        {
+            paranoid: true,
+        }
+    );
+};
