bb623d01245827c3fc215ca6d1eb12e54bbeaef1
nesmun <jesus.emc@hotmail.com>
2023-10-03 17:44:14 -0600

    reviews ordenados por fecha O por rating

--- src/Handlers/reviewHandler.js
+++ src/Handlers/reviewHandler.js
@@ -61,105 +61,118 @@ const handlePostReview = async (req, res) => {
 };
 
 const getAllReviews = async (req, res) => {
-  try {
-    const allReviews = await Review.findAll();
-    if (allReviews.length === 0) {
-      return res.status(400).json({ error: "No se encontró ninguna reseña" });
+    try {
+        const allReviews = await Review.findAll();
+        if (allReviews.length === 0) {
+            return res
+                .status(400)
+                .json({ error: "No se encontró ninguna reseña" });
+        }
+        return res.status(200).json(allReviews);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
     }
-    return res.status(200).json(allReviews);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
 };
 
 // Manejador para la ruta GET /reviewsByServiceId
 
 const handleGetReviewsByServiceId = async (req, res) => {
-  const { serviceId } = req.params;
+    const { serviceId } = req.params;
+    const { rating, date } = req.query;
 
-  try {
-    // Busca las revisiones asociadas al servicio por su ID
-    const reviews = await Review.findAll({
-      where: { service_id: serviceId },
-    });
-    if (reviews.length === 0) {
-      return res
-        .status(400)
-        .json({ error: "No se entontraron reseñas para este servicio" });
+    try {
+        // Busca las revisiones asociadas al servicio por su ID
+        let reviews = await Review.findAll({
+            where: { service_id: serviceId },
+        });
+
+        if (reviews.length === 0) {
+            return res.status(400).json({
+                error: "No se entontraron reseñas para este servicio",
+            });
+        }
+
+        if (rating) {
+            reviews = reviews.sort((a, b) => {
+                return a.rating - b.rating;
+            });
+        } else if (date) {
+            reviews = reviews.sort((a, b) => {
+                return a.createdAt - b.createdAt;
+            });
+        }
+
+        res.status(200).json(reviews);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
     }
-
-    res.status(200).json(reviews);
-  } catch (error) {
-    res.status(500).json({ error: "Error interno del servido y de la " });
-  }
 };
 
 // Manejador para la ruta GET /reviewsByUserId
 const handleGetReviewsByUserId = async (req, res) => {
-  const { userId } = req.params;
-  try {
-    const reviewsByUser = await Review.findAll({
-      where: { user_id: userId },
-    });
-    if (reviewsByUser.length === 0) {
-      return res
-        .status(200)
-        .json({ error: "El usuario no escribió ninguna reseña" });
+    const { userId } = req.params;
+    try {
+        const reviewsByUser = await Review.findAll({
+            where: { user_id: userId },
+        });
+        if (reviewsByUser.length === 0) {
+            return res
+                .status(200)
+                .json({ error: "El usuario no escribió ninguna reseña" });
+        }
+        return res.status(200).json(reviewsByUser);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
     }
-    return res.status(200).json(reviewsByUser);
-  } catch (error) {
-    return res.status(500).json({ error: error.message });
-  }
 };
 
 const handleReviewById = async (req, res) => {
-  const { id } = req.params;
-  try {
-    const review = await Review.findOne({
-      where: { id: id },
-    });
-    if (!review) {
-      return res.status(400).json({ error: "No se encontró la reseña" });
+    const { id } = req.params;
+    try {
+        const review = await Review.findOne({
+            where: { id: id },
+        });
+        if (!review) {
+            return res.status(400).json({ error: "No se encontró la reseña" });
+        }
+        return res.status(200).json(review);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
     }
-    return res.status(200).json(review);
-  } catch (error) {
-    return res.status(500).json({ error: error.message });
-  }
 };
 
 // Eliminar una reseña por su ID
 const handleDeleteReview = async (req, res) => {
-  try {
-    const { id } = req.params;
-    
+    try {
+        const { id } = req.params;
 
-    // Buscar la reseña por su ID
-    const review = await Review.findByPk(id);
+        // Buscar la reseña por su ID
+        const review = await Review.findByPk(id);
 
-    // Verificar si la reseña existe
-    if (!review) {
-      return res.status(404).json({ message: "Reseña no encontrada" });
+        // Verificar si la reseña existe
+        if (!review) {
+            return res.status(404).json({ message: "Reseña no encontrada" });
+        }
+
+        // Eliminar la reseña de forma física
+        await review.destroy({ force: true });
+
+        return res.status(200).json({ message: "Reseña eliminada con exito" }); // 204 significa "No Content"
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: error.message });
     }
-
-    // Eliminar la reseña de forma física
-    await review.destroy({ force: true });
-
-    return res.status(200).json({ message: "Reseña eliminada con exito" }); // 204 significa "No Content"
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ error: "Error al eliminar la reseña" });
-  }
 };
 
 module.exports = {
-  handleDeleteReview,
+    handleDeleteReview,
 };
 
 module.exports = {
-  handleGetReviewsByServiceId,
-  handleGetReviewsByUserId,
-  handlePostReview,
-  getAllReviews,
-  handleReviewById,
-  handleDeleteReview,
+    handleGetReviewsByServiceId,
+    handleGetReviewsByUserId,
+    handlePostReview,
+    getAllReviews,
+    handleReviewById,
+    handleDeleteReview,
 };
