bca2d349dc390739cfe2947631d5ea497d5725b6
jeermuce <ernesto_mc.dev@proton.me>
2024-09-06 02:09:21 -0600

    devfix: env-defined ssl db, DB_URL env-var for ease of change

--- src/db.js
+++ src/db.js
@@ -2,18 +2,20 @@ require("dotenv").config();
 const { Sequelize, Op } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
-
+const { DB_URL,SSL_ENABLED } = process.env;
+const ssl = SSL_ENABLED === "true";
 const sequelize = new Sequelize(
-    `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+    `${DB_URL}`,
     {
         logging: false, // set to console.log to see the raw SQL queries
         native: false, // lets Sequelize know we can use pg-native for ~30% more speed
         dialectOptions: {
             supportBigNumbers: true,
-        },
-    }
+            ssl: ssl, // disable SSL
+        }, 
+    }, 
 );
+
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
